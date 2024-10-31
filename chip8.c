5af9cfb8bf5865a0776435d762f0bd1e7267a5f4
dusk-beep <dusk10037@gmail.com>
2024-10-31 13:19:48 +0530

    imorovemnet to exit logic

--- chip8.c
+++ chip8.c
@@ -10,13 +10,23 @@ typedef struct {
 	SDL_Renderer* render;
 } sdl_t;
 
-
 typedef struct {
 	uint32_t window_width;
 	uint32_t window_height;
 	uint32_t for_color;
 	uint32_t back_color;
+	uint32_t scale_factor;
 } config_t;
+
+typedef enum {
+	QUIT,
+	RUNNING,
+	PAUSED,
+} emulator_state_t;
+
+typedef struct {
+	emulator_state_t state;
+} chip8_t;
 
 //sdl congiguration from command and wudth and window_height
 bool config_sdl(config_t* config) {
@@ -24,6 +34,7 @@ bool config_sdl(config_t* config) {
 	config->window_height = 32;
 	config->for_color = 0xFFFFFFFF;
 	config->back_color = 0xFF0000FF;
+	config->scale_factor = 15;
 	return true;
 }
 
@@ -38,17 +49,38 @@ void color_sdl(sdl_t sdl,config_t config) {
 	SDL_RenderClear(sdl.render);
 }
 
-//sdl update window 
-void update_sdl(sdl_t sdl) {
-  SDL_RenderPresent(sdl.render);
+//handle user inputs
+void handle_input(chip8_t* chip8) {
+	SDL_Event event;
+
+	while(SDL_PollEvent(&event)) {
+		switch (event.type) {
+		case SDL_QUIT :
+			chip8->state = QUIT; //the while loop will exit in main
+			return; //quit application when cross pressed
+			//add more keys later
+		}
+	}
 }
+
+//initialize the chip8
+bool init_chip8(chip8_t* chip8) {
+	chip8->state = RUNNING;
+	return true;
+}
+
+//sdl update window
+void update_sdl(sdl_t sdl) {
+	SDL_RenderPresent(sdl.render);
+}
+
 //sdl initialization
 bool init_sdl(sdl_t* sdl,const config_t config) {
 	if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER)!=0) {
 		SDL_Log("could not initialize sdl sub systems : %s ",SDL_GetError());
 		return false;
 	}
-	sdl->window = SDL_CreateWindow("chip8 emu",SDL_WINDOWPOS_CENTERED,                                          SDL_WINDOWPOS_CENTERED,config.window_width,                                 config.window_height,SDL_WINDOW_SHOWN);
+	sdl->window = SDL_CreateWindow("chip8 emu",SDL_WINDOWPOS_CENTERED,                                          SDL_WINDOWPOS_CENTERED,config.window_width*config.scale_factor,                                 config.window_height*config.scale_factor,SDL_WINDOW_SHOWN);
 
 	if(!sdl->window) {
 		SDL_Log("could not craete a window %s ",SDL_GetError());
@@ -80,32 +112,21 @@ int main(int argc, char *argv[])
 	//initialize emu congig
 	if(!config_sdl(&config)) return -1;
 
-	//start sdl
+	//sinitialize sdl
 	if(!init_sdl(&sdl,config)) return -1;
+
+	//initialize chip
+	chip8_t chip8 = {0};
+	if(!init_chip8(&chip8)) return -1;
 
 	//set the initial screen
 	color_sdl(sdl,config);
-  
-  SDL_Event event;
-    // Main emulator loop
-    bool running = true; // Control variable for the loop
-    while (running) {
-        // Handle events
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) { // Check for quit event (window close)
-                running = false; // Set running to false to exit loop
-            }
-            // Check for keyboard events
-            if (event.type == SDL_KEYDOWN) {
-                if (event.key.keysym.sym == SDLK_ESCAPE) { // Check if the Escape key is pressed
-                    running = false; // Set running to false to exit loop
-                }
-            }
-        }
-        SDL_Delay(16); // Delay for ~60 FPS
-        update_sdl(sdl); // Update the screen
-    }
 
+	while(chip8.state != QUIT) {
+		handle_input(&chip8); //if state paused then continue todo
+		SDL_Delay(16);
+		update_sdl(sdl);
+	}
 
 
 	cleanup_sdl(&sdl);
