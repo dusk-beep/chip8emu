c86e160220402785bbec97c314ccb237f4bbd174
dusk-beep <dusk10037@gmail.com>
2024-11-19 01:05:59 +0530

    modified lsp to work wiyh external files

--- chip8.c
+++ chip8.c
@@ -29,7 +29,8 @@ typedef struct {
 } chip8_t;
 
 //sdl congiguration from command and wudth and window_height
-bool config_sdl(config_t* config) {
+bool config_sdl(config_t* config)
+{
 	config->window_width = 64;
 	config->window_height = 32;
 	config->for_color = 0xFFFFFFFF;
@@ -39,22 +40,24 @@ bool config_sdl(config_t* config) {
 }
 
 //set wundow clear and color
-void color_sdl(sdl_t sdl,config_t config) {
-	const uint8_t r = (config.back_color >> 24)& 0xFF;
-	const uint8_t g = (config.back_color >> 16)& 0xFF;
-	const uint8_t b = (config.back_color >> 8)& 0xFF;
-	const uint8_t a = (config.back_color >> 0)& 0xFF;
+void color_sdl(sdl_t sdl, config_t config)
+{
+	const uint8_t r = (config.back_color >> 24) & 0xFF;
+	const uint8_t g = (config.back_color >> 16) & 0xFF;
+	const uint8_t b = (config.back_color >> 8) & 0xFF;
+	const uint8_t a = (config.back_color >> 0) & 0xFF;
 
-	SDL_SetRenderDrawColor(sdl.render,r,g,b,a);
+	SDL_SetRenderDrawColor(sdl.render, r, g, b, a);
 	SDL_RenderClear(sdl.render);
 }
 
 //handle user inputs
-void handle_input(chip8_t* chip8) {
+void handle_input(chip8_t* chip8)
+{
 	SDL_Event event;
 
 	while(SDL_PollEvent(&event)) {
-		switch (event.type) {
+		switch(event.type) {
 		case SDL_QUIT :
 			chip8->state = QUIT; //the while loop will exit in main
 			return; //quit application when cross pressed
@@ -64,63 +67,73 @@ void handle_input(chip8_t* chip8) {
 }
 
 //initialize the chip8
-bool init_chip8(chip8_t* chip8) {
+bool init_chip8(chip8_t* chip8)
+{
 	chip8->state = RUNNING;
 	return true;
 }
 
 //sdl update window
-void update_sdl(sdl_t sdl) {
+void update_sdl(sdl_t sdl)
+{
 	SDL_RenderPresent(sdl.render);
 }
 
 //sdl initialization
-bool init_sdl(sdl_t* sdl,const config_t config) {
-	if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER)!=0) {
-		SDL_Log("could not initialize sdl sub systems : %s ",SDL_GetError());
+bool init_sdl(sdl_t* sdl, const config_t config)
+{
+	if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0) {
+		SDL_Log("could not initialize sdl sub systems : %s ", SDL_GetError());
 		return false;
 	}
-	sdl->window = SDL_CreateWindow("chip8 emu",SDL_WINDOWPOS_CENTERED,                                          SDL_WINDOWPOS_CENTERED,config.window_width*config.scale_factor,                                 config.window_height*config.scale_factor,SDL_WINDOW_SHOWN);
+
+	sdl->window = SDL_CreateWindow("chip8 emu", SDL_WINDOWPOS_CENTERED,
+	                               SDL_WINDOWPOS_CENTERED, config.window_width * config.scale_factor,
+	                               config.window_height * config.scale_factor, SDL_WINDOW_SHOWN);
 
 	if(!sdl->window) {
-		SDL_Log("could not craete a window %s ",SDL_GetError());
+		SDL_Log("could not craete a window %s ", SDL_GetError());
 		return false;
 	}
 
-	sdl->render = SDL_CreateRenderer(sdl->window, -1,SDL_RENDERER_SOFTWARE);
+	sdl->render = SDL_CreateRenderer(sdl->window, -1, SDL_RENDERER_SOFTWARE);
 
 	if(!sdl->render) {
-		SDL_Log("could not create renderer %s ",SDL_GetError());
+		SDL_Log("could not create renderer %s ", SDL_GetError());
 	}
+
 	return true;
 }
 
 //sdl cleanup
-void cleanup_sdl(sdl_t* sdl) {
+void cleanup_sdl(sdl_t* sdl)
+{
 	SDL_DestroyRenderer(sdl->render);
 	SDL_DestroyWindow(sdl->window);
 	SDL_Quit();
 }
 
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 	(void)argc;
 	(void)argv;
 	sdl_t sdl = {0};
 	config_t config = {0};
+
 	//initialize emu congig
 	if(!config_sdl(&config)) return -1;
 
 	//sinitialize sdl
-	if(!init_sdl(&sdl,config)) return -1;
+	if(!init_sdl(&sdl, config)) return -1;
 
 	//initialize chip
 	chip8_t chip8 = {0};
+
 	if(!init_chip8(&chip8)) return -1;
 
 	//set the initial screen
-	color_sdl(sdl,config);
+	color_sdl(sdl, config);
 
 	while(chip8.state != QUIT) {
 		handle_input(&chip8); //if state paused then continue todo
@@ -128,7 +141,8 @@ int main(int argc, char *argv[])
 		update_sdl(sdl);
 	}
 
-
 	cleanup_sdl(&sdl);
 	return 0;
 }
+
+
